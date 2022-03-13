f172c4dac4518a08ca47b41ce993144cd21ead2f
Thelgaris <alex.gabarron84@gmail.com>
2022-03-13 13:35:26 +0000

    readytogo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
+  document.getElementById("escuse").innerHTML = "Hola";
+
+  document.querySelector("#escuse").innerHTML = escuse();
   console.log("Hello Rigo from the console!");
+};
+
+const escuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoShow = Math.floor(Math.random() * who.length);
+  let actionShow = Math.floor(Math.random() * action.length);
+  let whatShow = Math.floor(Math.random() * what.length);
+  let whenShow = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoShow] +
+    " " +
+    action[actionShow] +
+    " " +
+    what[whatShow] +
+    " " +
+    when[whenShow]
+  );
 };
